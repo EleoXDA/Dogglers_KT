6000daa86f4384c8b6c4702f346d38bc8840e29f
Elbay Malik <eleo.666@gmail.com>
2022-02-10 21:50:18 +0100

    kotlin edits and XML Layouts

--- app/src/main/java/eu/eleo/dogglers/adapter/DogCardAdapter.kt
+++ app/src/main/java/eu/eleo/dogglers/adapter/DogCardAdapter.kt
@@ -16,9 +16,14 @@
 package eu.eleo.dogglers.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import eu.eleo.dogglers.R
+import eu.eleo.dogglers.model.Dog
 
 /**
  * Adapter to inflate the appropriate list item layout and populate the view with information
@@ -29,30 +34,38 @@ class DogCardAdapter(
     private val layout: Int
 ): RecyclerView.Adapter<DogCardAdapter.DogCardViewHolder>() {
 
-    // TODO: Initialize the data using the List found in data/DataSource
-
-    /**
-     * Initialize view elements
+    /** TODO: Initialize the data using the List found in data/DataSource
+        Initialize view elements
      */
-    class DogCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
+    class DogCardViewHolder(private val view: View?): RecyclerView.ViewHolder(view!!) {
+        val textView: TextView = view?.findViewById(R.id.item_title) ?:
+        val imageView: ImageView = view?.findViewById(R.id.item_image) ?:
+
         // TODO: Declare and initialize all of the list item UI components
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogCardViewHolder {
+        when (layout) {
+            1 ->
+            2 ->
+            3 ->
+        }
         // TODO: Use a conditional to determine the layout type and set it accordingly.
         //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
         //  the vertical/horizontal list item should be used.
 
-        // TODO Inflate the layout
+        val adapterLayout = LayoutInflater.from(parent.context)
 
+        // TODO Inflate the layout
+            .inflate(R.layout.vertical_horizontal_list_item, parent, false)
         // TODO: Null should not be passed into the view holder. This should be updated to reflect
         //  the inflated layout.
-        return DogCardViewHolder(null)
+        return DogCardViewHolder(adapterLayout)
     }
 
-    override fun getItemCount(): Int = 0 // TODO: return the size of the data set instead of 0
+    override fun getItemCount(): Int = dataset.size
 
-    override fun onBindViewHolder(holder: DogCardViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: DogCardAdapter.DogCardViewHolder, position: Int) {
         // TODO: Get the data at the current position
         // TODO: Set the image resource for the current dog
         // TODO: Set the text for the current dog's name
--- app/src/main/java/eu/eleo/dogglers/model/Dog.kt
+++ app/src/main/java/eu/eleo/dogglers/model/Dog.kt
@@ -20,9 +20,6 @@ import androidx.annotation.DrawableRes
 /**
  * A data class to represent the information presented in the dog card
  */
-data class Dog(
-    @DrawableRes val imageResourceId: Int,
-    val name: String,
-    val age: String,
-    val hobbies: String
-)
+data class Dog (@DrawableRes val imageResourceId: Int, val name: String, val age: String,val hobbies: String){
+
+}
