b1f425d98933cf3cf9dc125aa307e64f28810bb0
Elbay Malik <eleo.666@gmail.com>
2022-02-10 17:44:59 +0100

    Initial commit

---
+++ app/src/main/java/eu/eleo/dogglers/HorizontalListActivity.kt
@@ -0,0 +1,44 @@
+/*
+* Copyright (C) 2021 The Android Open Source Project.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package eu.eleo.dogglers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.eleo.dogglers.adapter.DogCardAdapter
+import eu.eleo.dogglers.const.Layout
+import eu.eleo.dogglers.databinding.ActivityHorizontalListBinding
+
+class HorizontalListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHorizontalListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHorizontalListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.horizontalRecyclerView.adapter = DogCardAdapter(
+            applicationContext,
+            Layout.HORIZONTAL
+        )
+
+        // Specify fixed size to improve performance
+        binding.horizontalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
